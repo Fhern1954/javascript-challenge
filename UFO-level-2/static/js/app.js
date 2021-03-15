8db3de99e016aae83d9358b2c907268fc9fa798c
Fhern1954 <63937814+Fhern1955@users.noreply.github.com>
2021-03-15 15:34:02 -0400

    Advanced level 2 - will sort when all the items are entered but not when only some items are entered.

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -17,14 +17,12 @@ tableData.forEach(row => {
     }
 });
 
-var btnSearch = d3.select("#filter-btn");
-var btnReset = d3.select("#btnReset");
 
-var searchDate = d3.select("#searchDate");
-var searchCity = d3.select("#searchCity");
-var searchState = d3.select("#searchState");
-var searchCountry = d3.select("#searchCountry");
-var searchShape = d3.select("#searchShape");
+var userDate = d3.select("#datetime");
+var userCity = d3.select("#city");
+var userState = d3.select("#state");
+var userCountry = d3.select("#country");
+var userShape = d3.select("#shape");
 
 //Create HTML table
 function loadTableRows(tableData) {
@@ -50,48 +48,88 @@ function loadTableRows(tableData) {
 loadTableRows(tableData);
 
 //Event Listeners
+var button = d3.select("#filter-btn");
+var form = d3.select("#user-form");
+button.on("click", filterData);
+form.on("submit", filterData);
 
-btnSearch.on("click", function(event) { 
+// btnSearch.on("click", function(event) { 
     console.log("button clicked");
-    //Prevent refresh
-        //d3.event.preventDefault();
+    
 
-    //Get what the user searched for
-    // var searchedDate = d3.select("#datetime")searchDate.property("value");
-    // var searchedCity = searchCity.property("value");
-    // var searchedState = searchState.property("value");
-    // var searchedCountry = searchCountry.property("value");
-    // var searchedShape = searchShape.property("value");
+    // function filterData (tableData) {
+    //     //Prevent refresh
+    //     d3.event.preventDefault;
 
-    // var filteredData = tableData.filter(sighting => 
-    //     sighting.datetime === searchedDate &&
-    //     sighting.city === searchedCity &&
-    //     sighting.state === searchedState &&
-    //     sighting.country === searchedCountry &&
-    //     sighting.shape === searchedShape);
+    //     let filter = 0;
 
-    filteredData = tableData;
-    console.log(searchedDate);
+        // let searchDate = userDate.property("value");
+        // let searchCity = userCity.property("value").toLowerCase();
+        // let searchState = userState.property("value").toLowerCase();
+        // let searchCountry = userCountry.property("value").toLowerCase();
+        // let searchShape = userShape.property("value").toLowerCase();
 
-    if (searchedDate !== null) {
-        filteredData = filteredData.filter(sighting.datetime === searchedDate.property("value"))
-    }
-    if (searchedCity !== null) {
-        filteredData = filteredData.filter(sighting.city === searchedCity)
-    }
-    if (searchedState !== null) {
-        filteredData = filteredData.filter(sighting.state === searchedState)
-    }
-    if (searchedCountry !== null) {
-        filteredData = filteredData.filter(sighting.country === searchedCountry)
-    }
-    if (searchedShape !== null) {
-        filteredData = filteredData.filter(sighting.shape === searchedShape)
-    }
+        //only filter if user entered a value
+        // if (searchDate || searchCity || searchState || searchCountry || searchShape) {
+        //     filtered = 1;
 
-loadTableRows(filteredData);
-    })
+        // //use only the fields that have values entered and dynamically build up filter conditions
+        // let userArray = [["datetime", searchDate], ["city", searchCity], ["state", searchState], ["country", searchCountry], ["shape", searchShape]];
+        // let existingArray = userArray.filter(user => user[1] !== "");
+        // let condition = existingArray.map(arr => "obs" + arr[0] + "===" + "'" + arr[1] + "'").join(" && ");
+
+        //What the next line evaluates.
+        //let filteredTable = tableData.filter(obs =>(obs.datetime == userDate) &&
+        //                                         (obs.city == userCity) &&
+        //                                         (obs.state == userState) &&
+        //                                         (obs.country == userCountry) &&
+        //                                         (obs.shape == userShape) );
+
+    //         let filterData = tableData.filter(obs => eval(condition));
+            
+    //         tbody.html("");
+
+    //         filterData.forEach(row => {
+    //             tbody.append("tr");
+
+    //             for (key in row){
+    //                 const cell = tbody.appen("td");
+    //                 cell.text(row[key]);
+    //             }
+    //         })
+    // }
+    // }
+
+    function filterData (tableData) {
+        d3.event.preventDefault;
+        var filteredData = data;
         //console.log(filteredData);
+
+        if (userDate !== null) {
+            searchDate = userDate.property("value");
+            console.log(searchDate);
+            filteredData = filteredData.filter(sighting => sighting.datetime === searchDate);
+        }
+        if (userCity !== null) {
+            searchCity = userCity.property("value").toLowerCase();
+            filteredData = filteredData.filter(sighting => sighting.city === searchCity);
+        }
+        if (userState !== null) {
+            searchState = userState.property("value").toLowerCase();
+            filteredData = filteredData.filter(sighting => sighting.state === searchState);
+        }
+        if (userCountry !== null) {
+            searchCountry = userCountry.property("value").toLowerCase();
+            filteredData = filteredData.filter(sighting => sighting.country === searchCountry);
+        }
+        if (userShape !== null) {
+            searchShape = userShape.property("value").toLowerCase();
+            filteredData = filteredData.filter(sighting => sighting.shape === searchShape);
+        }
+        console.log(filteredData);
+    loadTableRows(filteredData);
+    }
+        
 
         //Create New Table
     //filteredData.forEach(function(selections){
